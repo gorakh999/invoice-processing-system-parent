617a21a4695c4f16dd3c3be26b3059c574a049bc
Gorakh <gorakhgupta999@gmail.com>
2024-10-22 23:41:44 +0530

    Reset master to an empty state

--- application/src/main/java/com/gorakh/projects/invoice/dao/InvoiceDAO.java
+++
@@ -1,8 +0,0 @@
-package com.gorakh.projects.invoice.dao;
-
-import com.gorakh.projects.invoice.model.Invoice;
-import org.springframework.stereotype.Service;
-
-public interface InvoiceDAO {
-    public Invoice getInvoiceByID(int id);
-}
--- application/src/main/java/com/gorakh/projects/invoice/service/InvoiceService.java
+++
@@ -1,9 +0,0 @@
-package com.gorakh.projects.invoice.service;
-
-import com.gorakh.projects.invoice.model.Invoice;
-import org.springframework.stereotype.Service;
-
-
-public interface InvoiceService {
-    public Invoice getInvoice(int id);
-}
